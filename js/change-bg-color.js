928717a0e537a84b7a8119e93fb5c974f3058df6
rafaweb <rafael.nunes@buser.com.br>
2019-08-31 14:02:30 -0300

    Add foto de perfil; Add conteúdo de nome, resumo, interesses e links sociais; Add ícones sociais; Add fonte Roboto;

--- js/change-bg-color.js
+++ js/change-bg-color.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    var bgcolorlist = new Array("#ef5350", "#ec407a", "#42a5f5", "#29b6f6", "#26c6da", "#26a69a", "#66bb6a", "#9ccc65", "#d4e157", "#ffee58", "#ffca28", "#ffa726", "#ff7043", "#bdbdbd")
-    document.body.style.background = bgcolorlist[Math.floor(Math.random() * bgcolorlist.length)]
+    var bgcolor = new Array("#ef5350", "#ec407a", "#42a5f5", "#29b6f6", "#26c6da", "#26a69a", "#66bb6a", "#9ccc65", "#d4e157", "#ffee58", "#ffca28", "#ffa726", "#ff7043", "#bdbdbd")
+    document.body.style.background = bgcolor[Math.floor(Math.random() * bgcolor.length)]
 }
